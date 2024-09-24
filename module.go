8612d247252fbb36c85b647cedae412a8795069c
Benji Rewis <benji.rewis@viam.com>
2024-09-24 10:36:03 -0400

    basic module

--- module.go
+++ module.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"context"
+
+	"go.viam.com/rdk/components/sensor"
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/module"
+
+	"go.viam.com/utils"
+)
+
+func main() {
+	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("socks-forwarder"))
+}
+
+func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) error {
+	sfModule, err := module.NewModuleFromArgs(ctx, logger)
+	if err != nil {
+		return err
+	}
+
+	err = sfModule.AddModelFromRegistry(ctx, sensor.API, sensorModel)
+	if err != nil {
+		return err
+	}
+
+	err = sfModule.Start(ctx)
+	if err != nil {
+		return err
+	}
+	defer sfModule.Close(ctx)
+
+	<-ctx.Done()
+	return nil
+}
