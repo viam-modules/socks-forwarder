005dd2e7ea94064ed8c739e507de77f9b9bc484a
Benji Rewis <benji.rewis@viam.com>
2024-10-07 19:50:27 +0000

    simpler entrypoint

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-
-# Install .deb package (`dpkg` command should be idempotent and should
-# automatically enable/start systemd service).
-dpkg -i ./socks-forwarder_0.1_arm64.deb
-
-go build ./
-exec ./socks-forwarder $@
