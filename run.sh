dc191c9e699d10608aeece046aeaccc9f2a0a950
Benji Rewis <benji.rewis@viam.com>
2025-03-14 12:23:08 -0400

    bump to version 0.5 of dpkg (adapter and MTU logs)

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 cd `dirname $0`
 
 echo "Installing the SOCKS forwarder systemd service if it has not already been installed"
-sudo dpkg -E -i ./socks-forwarder_0.4_arm64.deb
+sudo dpkg -E -i ./socks-forwarder_0.5_arm64.deb
 
 exec ./socks-forwarder-module $@
